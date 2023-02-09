66d2279f20756c85bcb085edf04a0cd7919d08a5
DINKONDBD <70340226+DINKONDBD@users.noreply.github.com>
2023-02-09 19:55:17 +0200

    aa

--- ItzPowerTerm/Program.cs
+++ ItzPowerTerm/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using NCalc;
+using System.Diagnostics;
 
 namespace ItzPowerTerm // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        public static string term_history = "";
         public static string usr_input = "";
         static void Main(string[] args)
         {
@@ -20,14 +22,41 @@ namespace ItzPowerTerm // Note: actual namespace depends on the project name.
         {
             Console.Write(">>");
             usr_input = Console.ReadLine();
-            switch(usr_input.Split(' ').FirstOrDefault())
+
+            switch(usr_input.Split(' ').FirstOrDefault().ToLower())
             {
                 default: Console.WriteLine("Command " + usr_input + " is not exists"); break;
+
                 case "calc":
-                    var result = new Expression(usr_input.Split(' ').Skip(1).FirstOrDefault()).Evaluate();
-                    Console.WriteLine(result);
+                    try
+                    {
+                        var result = new Expression(usr_input.Split(' ').Skip(1).FirstOrDefault()).Evaluate();
+                        Console.WriteLine(result);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("error was occured");
+                    }
+
                     break;
-            }
+
+                case "history":
+                    if (term_history != "") Console.WriteLine("Your terminal session history: \n" + term_history);
+                    break;
+
+                case "start":
+                    try
+                    {
+                        System.Diagnostics.Process.Start(usr_input.Split(' ').Skip(1).FirstOrDefault());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("error was occured");
+                    }
+                    
+                    break;
+            }   
+            term_history += DateTime.Now.ToString("h:mm:ss tt")+ " " + usr_input + "\n";
         }
     }
 }
