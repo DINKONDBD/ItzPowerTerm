dca1719b4136ba8578355058847f377af0764af4
DINKONDBD <70340226+DINKONDBD@users.noreply.github.com>
2023-02-11 19:56:07 +0200

    make code more readable

--- ItzPowerTerm/Program.cs
+++ ItzPowerTerm/Program.cs
@@ -14,7 +14,7 @@ namespace ItzPowerTerm // Note: actual namespace depends on the project name.
             //var secondWord = str.Split(' ').Skip(1).FirstOrDefault();
             Console.WriteLine("ItzPowerTerm v0.1 by DINKONDBD (dinkondbd.github.io)");
             string usr_input = "";
-            while(usr_input != "exit")
+            while (usr_input != "exit")
             {
                 Term();
             }
@@ -24,109 +24,99 @@ namespace ItzPowerTerm // Note: actual namespace depends on the project name.
             Console.Write(">>");
             input = Console.ReadLine();
 
-            switch(input.Split(' ').FirstOrDefault().ToLower())
+            switch (input.Split(' ').FirstOrDefault().ToLower())
             {
-                default: if(input != "") Console.WriteLine("Command " + input + " is not exists"); break;
+                default: if (input != "") Console.WriteLine("Command " + input + " is not exists"); break;
+
                 case "help":
-                    try
-                    {
-                        Console.WriteLine("Commands:" +
-                            "\n calc - Shows output evaluation of argument [1 args]" +
-                            "\n clear - clears console [0 args]" +
-                            "\n history - shows current terminal session history [0 args]" +
-                            "\n ls - shows subfolders or all files [2 args] arg -s show all subfolders -a shows all files (requires to type path) (example ls -a C:/) " +
-                            "\n start - starts file or program from argument [1 args]");
-                    }
-                    catch (System.Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+
+                    showHelp();
                     break;
 
                 case "calc":
-                    try
-                    {
-                        var result = new Expression(input.Split(' ').Skip(1).FirstOrDefault()).Evaluate();
-                        Console.WriteLine(result);
-                    }
-                    catch (System.Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                break;
+
+                    calc();
+
+                    break;
                 case "clear":
-                    try
-                    {
-                        Console.Clear();
-                    }
-                    catch (System.Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+
+                    clear();
 
                     break;
 
                 case "history":
-                    if (term_history != "") Console.WriteLine("Your terminal session history: \n" + term_history);
+
+                    showhistory();
+
                     break;
 
                 case "start":
-                    try
-                    {
-                        System.Diagnostics.Process.Start(input.Split(' ').Skip(1).FirstOrDefault());
-                    }
-                    catch (System.Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+
+                    start();
 
                     break;
                 case "ls":
-                    try
-                    {
-                        if (input.Split(' ').Skip(1).FirstOrDefault() == "-a")
-                        {
-                            if (input.Split(' ').Skip(2).FirstOrDefault() == "" || input.Split(' ').Skip(2).FirstOrDefault() == null)
-                            {
-                                Console.WriteLine("Type path to list all files");
-                            }
-                            else
-                            {
-                                DirectorySearch(input.Split(' ').Skip(2).FirstOrDefault());
-                            }
-                            
-                        }
-                        else if (input.Split(' ').Skip(1).FirstOrDefault() == "-s")
-                        {
-                            if (input.Split(' ').Skip(2).FirstOrDefault() == "" || input.Split(' ').Skip(2).FirstOrDefault() == null)
-                            {
-                                Console.WriteLine("Type path to list all sub folders");
-                            }
-                            else
-                            {
-                                string[] fileArray = Directory.GetDirectories(input.Split(' ').Skip(2).FirstOrDefault());
 
-                                for (int i = 0; i < fileArray.Length; i++)
-                                {
-
-                                    Console.WriteLine(fileArray[i]);
-                                }
-                            }
-
-                        }
-                        else
-                        {
-                            Console.WriteLine("parameter " + input.Split(' ').Skip(1).FirstOrDefault() + " was not found");
-                        }
-                    }
-                    catch (System.Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+                    ls();
 
                     break;
-            }   
-            term_history += DateTime.Now.ToString("h:mm:ss tt")+ " " + input + "\n";
+            }
+            term_history += DateTime.Now.ToString("h:mm:ss tt") + " " + input + "\n";
+        }
+
+        private static void calc()
+        {
+            try
+            {
+                var result = new Expression(input.Split(' ').Skip(1).FirstOrDefault()).Evaluate();
+                Console.WriteLine(result);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void showHelp()
+        {
+            try
+            {
+                Console.WriteLine("Commands:" +
+                    "\n calc - Shows output evaluation of argument [1 args]" +
+                    "\n clear - clears console [0 args]" +
+                    "\n history - shows current terminal session history [0 args]" +
+                    "\n ls - shows subfolders or all files [2 args] arg -s show all subfolders -a shows all files (requires to type path) (example ls -a C:/) " +
+                    "\n start - starts file or program from argument [1 args]");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private static void showhistory(){if (term_history != "") Console.WriteLine("Your terminal session history: \n" + term_history);}
+
+        private static void clear()
+        {
+            try
+            {
+                Console.Clear();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void start()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(input.Split(' ').Skip(1).FirstOrDefault());
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
         public static void DirectorySearch(string dir)
         {
@@ -140,6 +130,51 @@ namespace ItzPowerTerm // Note: actual namespace depends on the project name.
                 {
                     Console.WriteLine(Path.GetFileName(d));
                     DirectorySearch(d);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ls()
+        {
+            try
+            {
+                if (input.Split(' ').Skip(1).FirstOrDefault() == "-a")
+                {
+                    if (input.Split(' ').Skip(2).FirstOrDefault() == "" || input.Split(' ').Skip(2).FirstOrDefault() == null)
+                    {
+                        Console.WriteLine("Type path to list all files");
+                    }
+                    else
+                    {
+                        DirectorySearch(input.Split(' ').Skip(2).FirstOrDefault());
+                    }
+
+                }
+                else if (input.Split(' ').Skip(1).FirstOrDefault() == "-s")
+                {
+                    if (input.Split(' ').Skip(2).FirstOrDefault() == "" || input.Split(' ').Skip(2).FirstOrDefault() == null)
+                    {
+                        Console.WriteLine("Type path to list all sub folders");
+                    }
+                    else
+                    {
+                        string[] fileArray = Directory.GetDirectories(input.Split(' ').Skip(2).FirstOrDefault());
+
+                        for (int i = 0; i < fileArray.Length; i++)
+                        {
+
+                            Console.WriteLine(fileArray[i]);
+                        }
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("parameter " + input.Split(' ').Skip(1).FirstOrDefault() + " was not found");
                 }
             }
             catch (System.Exception ex)
